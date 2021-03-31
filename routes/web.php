d3f8b6ef12c7b6965a07a8a0f7c110183913893d
Nnamdi Okeke <nahorr@gmail.com>
2021-03-30 18:12:44 -0600

    frontend home

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::group(['namespace' => 'Frontend','as' => 'frontend.'], function () {
+
+        //Home
+		Route::get('/', [HomeController::class, 'home'])->name('home');
+
+
+});
+
