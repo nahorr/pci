f8400f5a4165b001c7f720439bad6ccd7c68af62
Nnamdi Okeke <nahorr@gmail.com>
2021-06-05 13:46:23 -0600

    changes

--- resources/views/frontend/make-donation.blade.php
+++ resources/views/frontend/make-donation.blade.php
@@ -2,6 +2,7 @@
 <html>
 
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PCI Strip Donation</title>
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
@@ -88,7 +89,7 @@
                                 </button>
                             </div>
                             <hr>
-                            <div class='form-row row text-center'>
+                            <div class="form-group">
                                 <img  class="img-fluid" src="{{asset('assets/img/stripe.png')}}" alt="stripe" width="480px" height="100px">
                             </div>
 
